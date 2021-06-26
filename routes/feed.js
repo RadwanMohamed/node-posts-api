9aa0cc614c064ad45bdf154b00947b4609e3dad9
radwan <radwan.mohamed@grandmstg.com>
2021-06-26 14:26:39 -0400

    node api

---
+++ routes/feed.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {body}  = require('express-validator');
+const router  = express.Router();
+const feedController = require('../controllers/feed');
+
+router.get('/posts',feedController.getPosts); 
+router.post('/posts',[
+    body('title').trim().isLength({min : 5}),
+   // body('body').trim().isLength({min : 5}),
+],feedController.createPost); 
+router.get('/posts/:postId',feedController.getPost); 
+
+router.put('/posts/:postId',
+[
+    body('title').trim().isLength({min : 5})
+]
+,feedController.updatePost); 
+
+router.delete('/posts/:postId',feedController.deletePost)
+module.exports = router; 
